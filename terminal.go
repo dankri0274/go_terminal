5caadcc9d23e9ce8ebbe552b44a5b7a3112696b1
dannyboy9934 <50834782+dannyboy9934@users.noreply.github.com>
2021-11-29 06:21:50 +0100

    Add files via upload

--- terminal.go
+++ terminal.go
@@ -1,28 +1,32 @@
 package main
 import (
-	"fmt"
 	"os"
+	"fmt"
+	"net"
+	"log"
 	"os/exec"
 	"runtime"
-	"time"
 )
 
 var clear map[string]func()
 
 func init() {
 	clear = make(map[string]func())
-	clear["linux"] = func() {						// FOR LINUX
+	clear["linux"] = func() {							//! <-- For Linux
 		cmd := exec.Command("clear")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
 	}
+	
 	clear["windows"] = func() {
-		cmd := exec.Command("cls", "/c", "cls")		// FOR WINDOWS
+		cmd := exec.Command("cmd", "/c", "cls")
 		cmd.Stdout = os.Stdout
 		cmd.Run()
+	}
+	
 }
 
-func CallClear() {
+func callClear() {
 	value, ok := clear[runtime.GOOS]
 	if ok {
 		value()
@@ -31,20 +35,40 @@ func CallClear() {
 	}
 }
 
+func getIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	return localAddr.IP
+}
+
 func main() {
 	var cmd string
 	var user string
 	
+	callClear()
+	
+	fmt.Print("Enter username: ")
+	fmt.Scanln(&user)
+	
+	fmt.Print("\033[H\033[2J")
+	
+	callClear() //! Clear terminal
+	
 	for cmd != "exit()" {
-		fmt.Print("Enter username: ")
-		fmt.Scanln(&user)
-
-		CallClear() // Clear terminal
-		
-		fmt.Print("Enter command: ")
+		fmt.Print(">>> ")
 		fmt.Scanln(&cmd)
 		if cmd == "whoami" {
 			fmt.Println("User", user)
+		} else if cmd == "exit()" {
+			os.Exit(0)
+		} else if cmd == "ip" {
+			fmt.Println(getIP())
 		} else {
 			fmt.Println("Only command available at the moment is \"whoami\"")
 		}
